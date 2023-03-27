ae31711832754ff1a80600185e2d9706e08d926d
Dinne Bosman <dwjbosman@yahoo.com>
2023-03-27 23:00:14 +0200

    Initial commit

---
+++ ServoControl/ServoController.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.IO.Ports;
+using System.Threading;
+
+namespace ServoControl;
+
+public delegate void ProcessReceivedMessageCallBack();
+
+public class ServoController
+{
+
+	enum ReadState {
+		ClientID,
+		Data,
+		Checksum,
+		Success,
+		Decoded,
+		Failure
+	}
+
+
+
+	private SerialPort serialPort;
+	private Thread statusMonitor;
+	private ReadState readerState = ReadState.ClientID;
+	private byte readerChecksum = 0;
+	private byte[] readerData = new byte[255];
+	private int readerDataPosition = 0;
+
+	private bool waitForEncoderResult = false;
+	private bool waitForStatusResult = false;
+	private bool stopMonitor = false;
+
+	public byte clientAddress { get; set; } = 0xE0;
+	public int errorCount { get; private set;} = 0;
+	
+	public int encoderRead { get; private set;} = 0;
+	public int position { get; private set; } =0;
+	public int encoderRequestInterval { get; set; } = 2000;
+	
+	public ServoController() {
+		serialPort = new SerialPort();	
+		statusMonitor = new Thread(new ThreadStart(Monitor));
+	}
+	
+
+	public void Open() {
+		serialPort.PortName = "/dev/ttyUSB0";
+		serialPort.BaudRate = 38400;
+ 		serialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+		serialPort.ReadTimeout = 0;
+		serialPort.Open();	
+		statusMonitor.Start();
+	}	
+
+	public void Close() {
+		serialPort.Close();
+		stopMonitor = true;
+		
+	}
+
+	private (int, bool, bool) processReceivedData(byte[] in_buffer, int expectedLength) {
+		int i = 0;
+		while (i<in_buffer.Length) {
+			if (readerState == ReadState.ClientID) {
+				if (in_buffer[i] == clientAddress) {
+					Console.WriteLine("Received client id");
+					readerChecksum = clientAddress;
+					readerDataPosition=0;
+					readerState = ReadState.Data;
+				}
+				i+=1;
+			} else if (readerState == ReadState.Data) {
+				if (readerDataPosition<expectedLength) {
+					Console.WriteLine($"Received data {readerDataPosition}/{expectedLength}");
+					readerData[readerDataPosition] = in_buffer[i];
+					readerChecksum += in_buffer[i];
+					readerDataPosition++;
+					i+=1;
+				} else {
+					readerState = ReadState.Checksum;
+					if (readerChecksum == in_buffer[i]) {
+						Console.WriteLine("Checksum ok");
+						readerState = ReadState.Success;
+						i+=1;
+						return (i, true, false);
+					} else {
+						Console.WriteLine("Checksum err");
+						errorCount+=1;
+						readerState = ReadState.Failure;
+						// do not advance buffer position
+						return (i, false, true);
+					}
+				}
+			}
+		}
+		return (i, false, false);
+	}
+
+	private void DataReceivedHandler(
+			object sender,
+			SerialDataReceivedEventArgs e)
+    {
+		SerialPort sp = (SerialPort)sender;
+		int avail = sp.BytesToRead;
+		Console.WriteLine("Data Received ", avail);
+		
+		byte[] in_buffer = new byte[avail];
+		int actual = sp.Read(in_buffer,0, avail);
+
+		Console.WriteLine("Data Read :", actual);
+		
+		if (waitForEncoderResult) {
+			(int pos, bool success, bool failure) = processReceivedData(in_buffer, 2);
+			if (success) {
+				processEncoderMessage();
+			}
+			if (failure) {
+				Console.WriteLine("Failed to read encoder message");
+			}
+		} else if (waitForStatusResult) {
+			(int pos, bool success, bool failure) = processReceivedData(in_buffer, 1);	
+			if (success) {
+				processStatusMessage();
+			}
+			if (failure) {
+				Console.WriteLine("Failed to read status message");
+			}
+		}
+	}
+
+	private void processEncoderMessage() {
+		waitForEncoderResult = false;
+		Console.WriteLine("Process enc message");
+		if (readerDataPosition==2) {
+			readerState = ReadState.Decoded;
+			Console.WriteLine($"Process encoder message: {readerData[0]},{readerData[1]}");
+			encoderRead+=1;
+		} else {
+			readerState = ReadState.Failure;
+			errorCount+=1;
+			Console.WriteLine("Process encoder message - incorrect length");
+		}
+	}
+
+	private void processStatusMessage() {
+		waitForStatusResult = false;
+		Console.WriteLine("Process status message");
+			
+		if (readerDataPosition==1) {
+			if (readerData[0] == 1) {
+				readerState = ReadState.Decoded;
+				Console.WriteLine("Process status message - success");
+			} else {
+				readerState = ReadState.Failure;
+				errorCount+=1;
+				Console.WriteLine("Process status - failure");
+			}
+		} else {
+			readerState = ReadState.Failure;
+			errorCount+=1;
+			Console.WriteLine("Process status message - incorrect length");
+		}
+	}
+	
+	public bool StopMotor() {
+		byte[] message = new byte[3];
+		message[0] = clientAddress;
+		message[1] = (byte)0xF7;
+		SetChecksum(message);
+		lock(this) {
+			readerState = ReadState.ClientID;
+			waitForStatusResult = true;
+
+			Console.WriteLine("Send Stop Motor");
+			serialPort.Write(message,0, message.Length);
+			return WaitForStatusResult();
+		}
+	}
+
+	public bool PowerEnable(bool ena) {
+		byte[] message = new byte[4];
+		message[0] = clientAddress;
+		message[1] = (byte)0xF3;
+		message[2] = ena ? (byte)1 : (byte)0;
+		SetChecksum(message);
+		lock(this) {
+			readerState = ReadState.ClientID;
+			waitForStatusResult = true;
+			
+			serialPort.Write(message,0, message.Length);		
+			return WaitForStatusResult();
+		}
+		
+	}
+
+	private byte CalculateChecksum(byte[] message) {
+		byte checksum = 0;
+		for (int i=0; i<message.Length-1; i++) {
+			checksum += message[i];
+		}
+		return checksum;
+	}
+
+	private void SetChecksum(byte[] message) {
+		byte checksum = CalculateChecksum(message);
+		message[message.Length-1] = checksum;
+	}
+
+	private bool CheckMessage(byte[] message) {
+		byte checksum = CalculateChecksum(message);
+		byte actualChecksum = message[message.Length-1];
+		return checksum == actualChecksum;
+	}
+
+	private bool WaitForStatusResult() {
+		int time = 0;
+		while ((time<5) && waitForStatusResult) {
+			Thread.Sleep(1000);
+			time += 1;
+		}	
+		if (waitForStatusResult) {
+			Console.WriteLine("Timeout reading status result");
+			waitForStatusResult = false;
+		}
+		return readerState == ReadState.Decoded;
+			
+	}
+
+	private bool WaitForEncoderResult() {
+		int time = 0;
+		
+		while ((time<5000) && waitForEncoderResult) {
+			Thread.Sleep(1);
+			time += 1;
+		}	
+		if (waitForEncoderResult) {
+			Console.WriteLine("Timeout reading encoder result");
+			waitForEncoderResult = false;
+		}
+		return readerState == ReadState.Decoded;
+	}
+
+
+	private void Monitor() {
+		while (!stopMonitor) {
+			if (serialPort.IsOpen) {
+				lock(this) {
+					RequestReadEncoder();
+					WaitForEncoderResult();
+				}
+			} else {
+				Console.WriteLine("Serial port closed");
+			}
+			Thread.Sleep(encoderRequestInterval);
+		}
+		Console.WriteLine("Exited monitor");
+	}
+
+	private void RequestReadEncoder() {
+		Console.WriteLine("Auto Request encoder");
+		byte[] message = new byte[3];
+		message[0] = clientAddress;
+		message[1] = 0x30;
+		SetChecksum(message);
+		
+		readerState = ReadState.ClientID;
+		waitForEncoderResult = true;
+
+		serialPort.Write(message,0, message.Length);
+			
+	}
+}
+
+
---
+++ ServoControlUtil/Program.cs
@@ -0,0 +1,94 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ServoControl;
+using System;
+using CommandLine;
+using System.Threading;
+
+namespace ServoControlUtil {
+
+	public class Options {
+		[Option('p', "port", Required = true, HelpText = "Serial port to connect to")]
+		public string Port { get; set; } = "/dev/ttyUSB0";
+
+		[Option('c', "command", Required = true, HelpText = "Motor command: Stop, Disable, Enable, GetPos")]
+		public string Command { get; set; } = "GetPos";
+
+		[Option('w', "wait", Required = false, HelpText = "  GetPos: keep reading encoder for 'wait' ms")]
+		public int Wait { get; set; } = 0;
+
+		[Option('e', "encoderRequestInterval", Required = false, HelpText = "Set the encoder request interval to x ms.")]
+		public int encoderRequestInterval { get; set; } = 0;
+	}
+
+	class Program {
+		static int Main(string[] args) {
+			Options o = Parser.Default.ParseArguments<Options>(args).Value;
+
+				Console.WriteLine($"Command: {o.Command}");
+
+				ServoController c = new ServoController();
+				Console.WriteLine("Wait1");
+				c.Open();
+				
+				try {
+					if (o.encoderRequestInterval !=0) {
+						c.encoderRequestInterval = o.encoderRequestInterval;	
+					}
+					if (o.Command == "Stop") {
+						Console.WriteLine("Stopping motor");
+						if (c.StopMotor()) {
+							Console.WriteLine("Motor stopped");
+							return 0;
+						} else {
+							Console.WriteLine("Failed to stop motor");
+							return 1;
+						}
+					}
+					if (o.Command == "Disable") {
+						Console.WriteLine("Disabling motor");
+						if (c.PowerEnable(false)) {
+							Console.WriteLine("Motor disabled");
+							return 0;
+						} else {
+							Console.WriteLine("Failed to disable motor");
+							return 1;
+						}
+					}
+					if (o.Command == "Enable") {
+						Console.WriteLine("Enabling motor");
+						if (c.PowerEnable(true)) {
+							Console.WriteLine("Motor enabled");
+							return 0;
+						} else {
+							Console.WriteLine("Failed to enable motor");
+							return 1;
+						}
+					}
+
+
+
+					if (o.Command == "GetPos") {
+						Console.WriteLine("Getting motor position");
+						int w = 0;
+						while ( ((c.encoderRead ==0) && (w<100)) || ((w*100)<o.Wait))  {
+							Thread.Sleep(100);
+							w+=1;
+						}
+						if (c.encoderRead == 0) {
+							Console.WriteLine("Failed to get encoder position");
+							return 1;
+						} else {
+							Console.WriteLine(c.position);
+							return 0;
+						}
+					}
+					Console.WriteLine("Unknown command");
+					return 1;	
+					//Console.WriteLine("Wait2");
+					//Thread.Sleep(5000);
+				} finally {
+					c.Close();
+				}
+		}
+	}
+}
