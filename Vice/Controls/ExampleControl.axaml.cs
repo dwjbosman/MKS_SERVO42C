6a969fb5363722cf9113f43ecb2811a431e6b7cb
Dinne Bosman <dinne.bosman@ns.nl>
2023-04-23 22:32:10 +0200

    Trying out templatedcontrols

---
+++ Vice/Controls/ExampleControl.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.Media;
+
+namespace Vice;
+
+public class ExampleControl : TemplatedControl
+{
+
+}
--- Vice/ViewModels/ViceControlViewModel.cs
+++ Vice/ViewModels/ViceControlViewModel.cs
@@ -1,10 +1,30 @@
 ﻿namespace Vice.ViewModels;
 using System;
 using Vice.Models;
+using ReactiveUI;
+
     public class ViceControlViewModel : ViewModelBase
     {
         public ViceControl Control { get; set; }
         public ViceControlViewModel(ViceControl control) {
                 Control = control;
+                gotoValue = "Test";
+        }
+
+        private string gotoValue;
+        public string GotoValue
+        {
+            get => gotoValue;
+            set => this.RaiseAndSetIfChanged(ref gotoValue, value);
+        }
+
+        public void UpdateGotoValue()
+        {
+            GotoValue = $"updated me at {DateTime.Now}";
+        }
+
+        public void NumberClicked(string param) {
+            //UpdateGotoValue();
+            GotoValue = param;
         }
     }
