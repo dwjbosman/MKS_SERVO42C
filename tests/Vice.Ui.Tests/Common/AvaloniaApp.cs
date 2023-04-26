f61a822d5e68a16fea3d4007b0b0587cea18ad16
Dinne Bosman <dinne.bosman@ns.nl>
2023-04-26 20:22:39 +0200

    First steps towards testing

---
+++ tests/Vice.Ui.Tests/Common/AvaloniaApp.cs
@@ -0,0 +1,48 @@
+using System;
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Headless;
+using Avalonia.ReactiveUI;
+using Avalonia.Threading;
+//using Camelot.Configuration;
+//using Camelot.DependencyInjection;
+using Vice.Views;
+using Splat;
+
+namespace Vice.Ui.Tests.Common;
+
+public static class AvaloniaApp
+{
+    public static void RegisterDependencies()
+    {
+        /** var config = new DataAccessConfiguration
+        {
+            UseInMemoryDatabase = true
+        };
+
+        Bootstrapper.Register(Locator.CurrentMutable, Locator.Current, config);**/
+    }
+
+    public static void Stop()
+    {
+        var app = GetApp();
+        if (app is IDisposable disposable)
+        {
+            Dispatcher.UIThread.Post(disposable.Dispose);
+        }
+
+        Dispatcher.UIThread.Post(() => app.Shutdown());
+    }
+
+    public static MainWindow GetMainWindow() => (MainWindow) GetApp().MainWindow;
+
+    public static IClassicDesktopStyleApplicationLifetime GetApp() =>
+        (IClassicDesktopStyleApplicationLifetime) Application.Current.ApplicationLifetime;
+
+    public static AppBuilder BuildAvaloniaApp() =>
+        AppBuilder
+            .Configure<App>()
+            .UsePlatformDetect()
+            .UseReactiveUI()
+            .UseHeadless();
+}
---
+++ tests/Vice.Ui.Tests/Common/Keyboard.cs
@@ -0,0 +1,29 @@
+using Avalonia.Input;
+using Avalonia.Input.Raw;
+
+namespace Vice.Ui.Tests.Common;
+
+public static class Keyboard
+{
+    public static void PressKey(IInputRoot inputRoot, Key key, RawInputModifiers modifiers = RawInputModifiers.None)
+    {
+        KeyDown(inputRoot, key, modifiers);
+        KeyUp(inputRoot, key, modifiers);
+    }
+
+    public static void KeyDown(IInputRoot inputRoot, Key key, RawInputModifiers modifiers = RawInputModifiers.None)
+    {
+        KeyboardDevice.Instance.ProcessRawEvent(
+            new RawKeyEventArgs(
+                KeyboardDevice.Instance, 0, inputRoot,
+                RawKeyEventType.KeyDown, key, modifiers));
+    }
+
+    public static void KeyUp(IInputRoot inputRoot, Key key, RawInputModifiers modifiers = RawInputModifiers.None)
+    {
+        KeyboardDevice.Instance.ProcessRawEvent(
+            new RawKeyEventArgs(
+                KeyboardDevice.Instance, 0, inputRoot,
+                RawKeyEventType.KeyUp, key, modifiers));
+    }
+}
