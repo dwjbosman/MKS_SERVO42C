1fb1811d42834c040b99f42a6f463d916dd953d2
Dinne Bosman <dinne.bosman@ns.nl>
2023-05-13 21:16:16 +0200

    Pipeline

--- Vice/Models/ViceControl.cs
+++ Vice/Models/ViceControl.cs
@@ -1,23 +1,67 @@
 using ReactiveUI;
 using ServoControl;
+//using System.Reactive.Core;
+//using System.Reactive.Interfaces;
+using System.Reactive.Linq;
+using System;
 
 namespace Vice.Models
 {
     public class ViceControl : ReactiveObject
     {
         private IServoController servoController;
-        private int position = 0;
+        
+        private bool _isCalibrated = false;
+        private int _position = 0;
+        
+        public int Position {
+            get => _position;
+            set => this.RaiseAndSetIfChanged(ref _position, value);
+        }
+
+        private bool _powerEnable = false;
+        
+        public bool PowerEnable {
+            get => _powerEnable;
+            set => this.RaiseAndSetIfChanged(ref _powerEnable, value);
+        }
+
         public string Status { get; set; }
 
-        public ViceControl() { 
+        private readonly ObservableAsPropertyHelper<bool> _powerEnabled;
+        public bool PowerEnabled => _powerEnabled.Value;
+
+        public ViceControl() {
+            servoController = new ServoControllerSimulator();
             Position = 30;
-            Status = "Unknown";
+            Status = "Idle";
+
+            _powerEnabled = this.WhenAnyValue(x => x.PowerEnable)
+            .Select(powerEnableValue => TrySetPowerEnable(powerEnableValue))
+            .ObserveOn(RxApp.MainThreadScheduler)
+            .ToProperty(this, x => x.PowerEnabled);
+
         }
 
+    private bool TrySetPowerEnable(bool powerEnableValue)
+    {
+                    servoController.PowerEnable(powerEnableValue); 
+                    bool nw = false; 
+                    servoController.GetPowerEnable(out nw);
+                    return nw;
+    }
 
-        public int Position {
-            get => position;
-            set => this.RaiseAndSetIfChanged(ref position, value);
+ 
+
+
+        public void startCalibrate() {
+            Status = "Calibrating";
+            
+            servoController.PowerEnable(true);
+
         }
+
+        
+
     }
 }
